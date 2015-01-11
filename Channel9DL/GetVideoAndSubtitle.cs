3b2dd5e06a128349194f459a68e29bab686df655
xJoker <wwj0110@qq.com>
2015-01-11 12:52:56 +0800

    GUI还没写 库写了

---
+++ Channel9DL/GetVideoAndSubtitle.cs
@@ -0,0 +1,83 @@
+﻿using HtmlAgilityPack;
+using System;
+
+
+namespace Channel9DL
+{
+    class GetVideoAndSubtitle
+    {
+
+        HtmlWeb webClient = new HtmlWeb();
+
+        HtmlDocument videoUrl = null; 
+
+        HtmlNodeCollection hrefList = null;//XPath
+
+        public HtmlDocument VideoUrl
+        {
+            get { return videoUrl; }
+            set { videoUrl = value; }
+        }
+
+        /// <summary>
+        /// 获取视频地址
+        /// </summary>
+        /// <param name="url">视频所在的网页</param>
+        /// <returns>返回完整的视频地址，如果没有地址会返回空值</returns>
+        public string GetVideoAddress(string url)
+        {
+            //加载需要分析的网页....
+            videoUrl = webClient.Load(url);
+
+            //XPath匹配
+            hrefList = videoUrl.DocumentNode.SelectNodes("//*[@id='video-download']/ul/li[4]/div/a");
+
+            //无结果会为空值
+            if (hrefList != null)
+            {
+                foreach (HtmlNode href in hrefList)
+                {
+                    HtmlAttribute att = href.Attributes["href"];
+                    Console.WriteLine("视频地址(High MP4)：");
+                    Console.WriteLine(att.Value);
+                    return att.Value;
+                }
+
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 获取字幕地址
+        /// </summary>
+        /// <param name="url">视频所在的网页</param>
+        /// <returns>返回完整的视频地址，如果没有地址会返回空值</returns>
+        public string GetSubtitleAddress(string url)
+        {
+            //加载需要分析的网页....
+            videoUrl = webClient.Load(url);
+
+            //XPath匹配
+            hrefList = videoUrl.DocumentNode.SelectNodes(".//a[@data-language='zh-cn']");
+
+            //无结果会为空值
+            if (hrefList != null)
+            {
+                foreach (HtmlNode href in hrefList)
+                {
+                    HtmlAttribute att = href.Attributes["href"];
+                    Console.WriteLine("字幕：");
+                    Console.WriteLine(att.Value);
+                    return att.Value;
+                }
+
+            }
+
+            return null;
+        }
+
+
+     
+    }
+}
---
+++ Channel9DL/SubtitleMake.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Channel9DL
+{
+    public class SubtitleMake
+    {
+        /// <summary>
+        /// 下载字幕并且处理
+        /// </summary>
+        /// <param name="url">字幕的地址</param>
+        /// <param name="name">视频名称</param>
+        /// <param name="language">传入要下载的字幕语言种类，默认简中</param>
+        public SubtitleMake(string url,string name,string language="zh-cn")
+        {
+
+            WebClient client = new WebClient();
+
+            try
+            {
+                System.Collections.Specialized.NameValueCollection VarPar = new System.Collections.Specialized.NameValueCollection();
+
+                //POST参数，这个将返回简体中文的字幕
+                VarPar.Add("l", language);
+
+                //获取返回的数据
+                byte[] remoteInfo = client.UploadValues(url, "POST", VarPar);
+
+                //解码为string类型
+                string sRemoteInfo = System.Text.Encoding.UTF8.GetString(remoteInfo);
+
+                //将所有的","替换为SRT字幕要求的","
+                sRemoteInfo = sRemoteInfo.Replace(".", ",");
+
+                //转为流
+                byte[] str = Encoding.UTF8.GetBytes(sRemoteInfo);
+                MemoryStream stream = new MemoryStream(str);
+
+                //利用StringBuilder做文件的分析存储容器
+                StringBuilder sbSRT = new StringBuilder();
+
+                StreamReader sr = new StreamReader(stream);
+
+                //已经读取的行数
+                int lineCount = 0;
+
+                //实际的行数(也就是去掉前三行无关的信息)
+                int line = 1;
+
+                while (sr.Peek() > 0)
+                {
+                    lineCount++;
+                    if (lineCount > 3)
+                    {
+                        sbSRT.AppendLine(line++.ToString());//字幕标号
+
+                        //读取3行
+                        sbSRT.AppendLine(sr.ReadLine());//时间戳
+                        sbSRT.AppendLine(sr.ReadLine());//字幕内容
+                        sbSRT.AppendLine(sr.ReadLine());//空行
+                    }
+                    else
+                    {
+                        //去掉前三行无关的信息
+                        sr.ReadLine();
+                    }
+
+                }
+
+                //存储在程序的目录下，和视频文件同名
+                string savePath = System.Environment.CurrentDirectory + name + ".srt";
+
+                StreamWriter sw = new StreamWriter(savePath, true, System.Text.Encoding.UTF8);
+                sw.Write(sbSRT);
+                sw.Close();
+
+                Console.WriteLine("字幕下载完毕");
+            }
+            catch
+            {
+
+                System.Windows.Forms.MessageBox.Show("获取字幕失败！可能这个视频没有简体字幕！");
+            }
+        }
+
+    }
+}
